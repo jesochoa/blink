4af045cdf4984d26dfe76699626bf859f83c186a
jesochoa <jsochoa8@gmail.com>
2021-01-25 14:51:12 +0100

    Comentarios de la funcion en define y del led interno

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include <Arduino.h>
-#define segundos(x) x * 1000
+#define segundos(x) x * 1000 //Convierto los milisegundos a segundos
 
 void setup()
 {
-  // put your setup code here, to run once:
-  //inicializo el pin13
+  //inicializo el pin13 que esta en la placa
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH); //enciendo el Led
   delay(segundos(1));
   digitalWrite(LED_BUILTIN, LOW); //apago el Led
